bdeb0e2a887d8d4bc3ce8234adfb93ac455e4ba0
Douglas Moraes <douglasgm89@live.com>
2024-06-12 22:06:08 -0300

    Introduction update

--- src/assets/components/intro/Introduction.jsx
+++ src/assets/components/intro/Introduction.jsx
@@ -1,52 +1,124 @@
-import React, { useContext, useEffect, useState } from 'react'
-import './introduction.css'
-import { ThemeContext } from '../../context/ThemeContext'
+import React, { useContext, useEffect, useState } from 'react';
+import './introduction.css';
+import { ThemeContext } from '../../context/ThemeContext';
+import Photo from '../../images/foto.jpg';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+const InfoModal = ({ show, onHide }) => {
+    const { lightTheme } = useContext(ThemeContext);
+
+    return (
+
+        <Modal
+            show={show}
+            onHide={onHide}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered>
+            <Modal.Header className={` ${lightTheme ? 'theme__light' : 'theme__dark'}`} closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    <div className={` ${lightTheme ? 'theme__light' : 'theme__dark'}`}>
+                        Sobre
+                    </div>
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className={` ${lightTheme ? 'theme__light' : 'theme__dark'}`}>
+                <h4>Centered Modal</h4>
+                <p>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                    consectetur ac, vestibulum at eros.
+                </p>
+            </Modal.Body>
+            <Modal.Footer className={` ${lightTheme ? 'theme__light' : 'theme__dark'}`}>
+                {/* <Button  onClick={onHide}>Close</Button> */}
+                <div className={lightTheme ? 'pd__introduction-connect-light' : 'pd__introduction-connect-dark'}>
+                    <a
+                        onClick={onHide}
+                        href='#'
+                        className="text-reset text-decoration-none">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <p>Ocultar</p>
+                    </a>
+                </div>
+            </Modal.Footer>
+        </Modal >
+    );
+};
 
 const Introduction = () => {
-    const { lightTheme } = useContext(ThemeContext)
-    const [text, setText] = useState('')
-    const toRotate = ['Douglas Moraes!   ', 'Desenvolvedor Front-End!   ']
-    const [loop, setLoop] = useState(0)
-    const [isDeleting, setIsDeleting] = useState(false)
-    const deletPeriod = 30
-    const writePeriod = 100
-    const [delta, setDelta] = useState(100)
+    const { lightTheme } = useContext(ThemeContext);
+    const [text, setText] = useState('');
+    const toRotate = ['Douglas Moraes!   ', 'Desenvolvedor Front-End!   '];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const deletPeriod = 30;
+    const writePeriod = 100;
+    const [delta, setDelta] = useState(100);
+    const [modalShow, setModalShow] = useState(false);
 
     useEffect(() => {
         let ticker = setInterval(() => {
-            toType()
-        }, delta)
-        return () => { clearInterval(ticker) }
-    }, [text])
+            toType();
+        }, delta);
+        return () => clearInterval(ticker);
+    }, [text]);
 
     const toType = () => {
         let i = loop % toRotate.length;
-        let fullText = toRotate[i]
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
-        setText(updatedText)
+        setText(updatedText);
 
         if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true)
-            setDelta(deletPeriod)
+            setIsDeleting(true);
+            setDelta(deletPeriod);
         } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false)
-            setDelta(writePeriod)
-            setLoop(loop + 1)
+            setIsDeleting(false);
+            setDelta(writePeriod);
+            setLoop(loop + 1);
         }
-
-    }
+    };
 
     return (
         <div className={`pd__introduction section__padding ${lightTheme ? 'theme__light' : 'theme__dark'}`}>
-            <h4><strong>Bem vindo ao meu Portifólio</strong></h4>
-            <h1>Olá, eu sou <span className={lightTheme ? 'highlight-light' : 'highlight-dark'}>{text}</span></h1>
-            <div className="pd__introduction-text">
-                <p>Sou um apaixonado por tecnologia e soluções inovadoras. Como desenvolvedor front-end, eu tenho o compromisso de entender as necessidades e desenvolver as melhores soluções com o objetivo de trazer resultados excepcionais para os negócios. Já desenvolvi projetos para otimizar processos administrativos e aumentar a rentabilidade de empreendedores. Estou semppre em busca de novos desafios e oportunidades, para aperfeiçoar e demonstrar todo o meu potencial.</p>
-                <button className={lightTheme ? 'button__light' : 'button__dark'} type='button'>Conecte-se comigo!</button>
+            <div className='pd__introduction-heading'>
+                <h4><strong>Bem vindo ao meu Portifólio</strong></h4>
+                <h1>Olá, eu sou <span className={lightTheme ? 'highlight-light' : 'highlight-dark'}>{text}</span></h1>
             </div>
-        </div>
-    )
-}
+            <div className="pd__introduction-content">
+                <div className='pd__introduction-content_text'>
+                    <p>Sou um apaixonado por tecnologia e soluções inovadoras. Como desenvolvedor front-end, eu tenho o compromisso de entender as necessidades e desenvolver as melhores soluções com o objetivo de trazer resultados excepcionais para os negócios. Já desenvolvi projetos para otimizar processos administrativos e aumentar a rentabilidade de empreendedores. Estou semppre em busca de novos desafios e oportunidades, para aperfeiçoar e demonstrar todo o meu potencial.</p>
+                </div>
+                <div className='pd__introduction-content_img'>
+                    <img src={Photo} alt="Minha foto" />
+                </div>
+            </div>
+            <div className='pd__introduction-content_btn'>
+                <div className={lightTheme ? 'pd__introduction-connect-light' : 'pd__introduction-connect-dark'}>
+                    <a
+                        onClick={(e) => { e.preventDefault(); setModalShow(true); }}
+                        href='#'
+                        className="text-reset text-decoration-none">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <p>Me conheça melhor!</p>
+                    </a>
+                </div>
+            </div>
 
-export default Introduction
+            <InfoModal
+                show={modalShow}
+                onHide={() => setModalShow(false)} />
+        </div>
+    );
+};
+
+export default Introduction;
