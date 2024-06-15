04d4624ebe1f95a9fcd820077ad9dd7378f9f41b
Douglas Moraes <douglasgm89@live.com>
2024-06-15 18:13:54 -0300

    CV Update

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -32,8 +32,8 @@ const Footer = () => {
                 <div className="pd__footer-right_cv">
                     <h1>Meu Curriculum</h1>
                     <div className="pd__footer-right_cv-btns">
-                        <Btndefault text="Download" link="public\files\Curriculo Douglas Gomes Moraes.pdf" download="Douglas Gomes Moraes.pdf" />
-                        <Btndefault text="Visualizar" link="public\files\Curriculo Douglas Gomes Moraes.pdf" />
+                        <Btndefault text="Download" link="public\files\curriculo.pdf" download="curriculo.pdf" />
+                        <Btndefault text="Visualizar" link="public\files\curriculo.pdf" />
                     </div>
                 </div>
                 <div className={`pd__footer-right_links ${lightTheme ? 'light' : 'dark'}`}>
